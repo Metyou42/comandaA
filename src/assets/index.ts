26d6d3769ad9b43d5f1eaca65ead7b276ebeb96a
Metyou42 <metyou42.m.a.a@gmail.com>
2023-11-11 21:30:24 +0200

    Init

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,25 @@
 import Logo from "assets/images/logo.png";
+import Cat from "assets/images/cat.png";
+import Study from "assets/images/Study.png";
+import Notebook from "assets/images/Notebook.png";
+import Group from "assets/images/Group.png";
+import Schedule from "assets/images/Schedule.png";
+import Search from "assets/images/Search.png";
+import Contact from "assets/images/Contact.png";
+import Settings from "assets/images/Settings.png";
+import LogOut from "assets/images/LogOut.png";
 
 export {
     Logo,
+    Cat,
+    Study,
+    Notebook,
+    Group,
+    Schedule,
+    Search,
+    Contact,
+    Settings,
+    LogOut
 };
 
 
---
+++ src/pages/Notes/index.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import { AppBar, Box, Button, Checkbox, FormControlLabel, Link, TextField } from "@mui/material";
+import { BlockFlex, BlockFlexCenter } from "./styled";
+import { BoxLogin } from "ui-components/BoxLogin/BoxLogin";
+import { PanelHeader } from "components/header";
+
+export function Notes(): React.ReactElement {
+
+    return (
+        <div>
+            <PanelHeader
+            />
+        </div>
+    );
+}
+
