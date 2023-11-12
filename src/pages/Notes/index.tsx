771f6e14b20e8b0d172b5b292709586e29f27e7b
oleh <oleglavro6@gmail.com>
2023-11-12 18:50:13 +0200

    content in notes

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -5,36 +5,45 @@ import { PanelHeader } from "components/header";
 import { MainBackGround } from "ui-components/MainCss/MainCSS";
 import { MainContainer } from "ui-components/MainContainer/MainContainer";
 
+
+import { Stack, Typography , Paper } from '@mui/material';
+
 export function Notes(): React.ReactElement {
+
+    const paperStyle = {
+        backgroundColor: 'rgba(83, 59, 119, 0.5)',
+        padding: '10px',
+        paddingLeft: '20px', // Adjust the left padding as needed
+        paddingRight: '20px', // Adjust the right padding as needed
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        //height: '100%', // This ensures the content takes up the full height
+        height: '50px'
+    };
+
 
     return (
         <MainBackGround>
             <PanelHeader />
 
             <MainContainer>
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Email"
-                    defaultValue="Email"
-                    sx={{
-                        fontSize: "1.6vh",
-                        width: "100%",
-                        margin: "23px 0 0 0"
-                    }}
-                />
+            <h3 style={{ textAlign: 'center', color: 'white' }}>
+                Archive
+            </h3>
 
-                <TextField
-                    required
-                    id="outlined-required"
-                    label="Password"
-                    defaultValue="Password"
-                    sx={{
-                        fontSize: "1.6vh",
-                        width: "100%",
-                        margin: "23px 0 0 0"
-                    }}
-                />
+            <Stack spacing={2} style={{padding: '10px', paddingLeft: '20px', paddingRight: '20px' }}>
+            <Paper style={paperStyle}>
+                <Typography>27.08 - Дискретна метематика - 5 лаб</Typography>
+            </Paper>
+            <Paper style={paperStyle}>
+                <Typography>27.08 - Дискретна метематика - 2 лаб</Typography>
+            </Paper>
+            <Paper style={paperStyle}>
+                <Typography>27.08 - Дискретна метематика - 3 лаб</Typography>
+            </Paper>
+            </Stack>
+
             </MainContainer>
         </MainBackGround>
     );
